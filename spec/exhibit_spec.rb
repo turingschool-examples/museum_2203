abc04f2a67df7fa3ed6ee1de6eb2fb4cce857994
Colin Reinhart <colinbreinhartgmail.com>
2022-04-13 09:47:20 -0600

    Add attributes test

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -3,11 +3,18 @@ require './lib/exhibit'
 require 'pry'
 
 describe Exhibit do
-
-
+  before :each do
+    @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+  end
 
   it 'exists' do
-    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
-    expect(exhibit).to be_an_instance_of(Exhibit)
+
+    expect(@exhibit).to be_an_instance_of(Exhibit)
+  end
+
+  it 'has attributes' do
+
+    expect(@exhibit.name).to eq("Gems and Minerals")
+    expect(@exhibit.cost).to eq(0)
   end
 end
