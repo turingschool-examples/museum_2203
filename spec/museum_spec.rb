65b52d824eb96f65769aa787e6a6e652ecbc17a5
Stephen Wilkens <stephenwilkens@gmail.com>
2022-04-13 10:42:58 -0600

    Test:add lottery test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -7,7 +7,7 @@ describe Museum do
   let(:gems_and_minerals){Exhibit.new({name: "Gems and Minerals", cost: 0})}
   let(:dead_sea_scrolls){Exhibit.new({name: "Dead Sea Scrolls", cost: 10})}
   let(:imax){Exhibit.new({name: "IMAX",cost: 15})}
-  let(:patron_1){Patron.new("Bob", 20)}
+  let(:patron_1){Patron.new("Bob", 0)}
   let(:patron_2){Patron.new("Sally", 20)}
   let(:patron_3){Patron.new("Johnny", 5)}
 
@@ -54,7 +54,7 @@ describe Museum do
     expect(dmns.patrons).to eq([patron_1, patron_2, patron_3])
   end
 
-  it "can give patrons by interest" do
+  it "can return patrons by interest" do
     dmns.add_exhibit(gems_and_minerals)
     dmns.add_exhibit(dead_sea_scrolls)
     dmns.add_exhibit(imax)
@@ -66,7 +66,23 @@ describe Museum do
     dmns.admit(patron_2)
     dmns.admit(patron_3)
     expect(dmns.patrons_by_exhibit_interest).to eq(gems_and_minerals => [patron_1],
-    dead_sea_scrolls => [patron_1, patron_2, patron_3], 
+    dead_sea_scrolls => [patron_1, patron_2, patron_3],
     imax => [])
+  end
+
+  it "can determine lotter contestants and pull a winner" do
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_2.add_interest("Dead Sea Scrolls")
+    patron_3.add_interest("Dead Sea Scrolls")
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+    expect(dmns.ticket_lottery_contestants(dead_sea_scrolls)).to eq([patron_1, patron_3])
+    expect(dmns.draw_lottery_winner(dead_sea_scrolls)).to eq("Johnny" || "Bob")
+    expect(dmns.draw_lottery_winner(gems_and_minerals)).to eq(nil)
   end
 end
