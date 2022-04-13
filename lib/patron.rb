468aa787060ea1b9dee1bb25525ef396467bd585
Andrew King <dev.andrewking@gmail.com>
2022-04-13 10:36:47 -0500

    add init to patron

--- lib/patron.rb
+++ lib/patron.rb
@@ -1,5 +1,15 @@
 class Patron
 
+  class Exhibit
+  attr_reader :name, :spending_money
 
+  def initialize(name, spending_money)
+    @name = name
+    @spending_money = spending_money
+  end
+
+
+
+  end
 
 end
