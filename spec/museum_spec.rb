4a9d9f5ab8821ef38c98f3aa55ebf7189bca27c2
Nick Jacobs <nickjacobsatc@gmail.com>
2022-04-13 11:39:01 -0500

    Test: makes hash of patrons by exhibit interest

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -26,7 +26,7 @@ RSpec.describe Museum do
     expect(dmns.exhibits.length).to eq(3)
   end
 
-  it " " do
+  it "can reccommend exhibits " do
     dmns = Museum.new("Denver Museum of Nature and Science")
     patron_1 = Patron.new("Bob", 20)
     patron_2 = Patron.new("Sally", 20)
@@ -35,8 +35,52 @@ RSpec.describe Museum do
     patron_2.add_interest("IMAX")
     expect(dmns.reccommend_exhibits(patron_1).length).to eq(2)
     expect(dmns.reccommend_exhibits(patron_2).length).to eq(1)
-    
+  end
 
+  it "can admit patrons regardless of money" do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    expect(dmns.patrons).to eq([])
+    patron_1 = Patron.new("Bob", 0)
+    patron_2 = Patron.new("Sally", 20)
+    patron_3 = Patron.new("Johnny", 5)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_2.add_interest("Dead Sea Scrolls")
+    patron_3.add_interest("Dead Sea Scrolls")
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+    expect(dmns.patrons.length).to eq(3)
+  end
+
+  it "makes hash of patrons by exhibit iterests " do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    expect(dmns.patrons).to eq([])
+    patron_1 = Patron.new("Bob", 0)
+    patron_2 = Patron.new("Sally", 20)
+    patron_3 = Patron.new("Johnny", 5)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_2.add_interest("Dead Sea Scrolls")
+    patron_3.add_interest("Dead Sea Scrolls")
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+    # require 'pry';binding.pry
+    expect(dmns.patrons_by_exhibit_interest).to be_a(Hash)
+    expect(dmns.patrons_by_exhibit_interest.keys).to eq([gems_and_minerals,dead_sea_scrolls,imax])
   end
 
 end
