f00fa23d721b074047f5af26a941cea01a514674
Eric Espindola <eric_e_2012@hotmail.com>
2022-04-13 08:13:38 -0700

    create tests for exhibit class

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -6,4 +6,15 @@ describe Exhibit do
     exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
     expect(exhibit).to be_an_instance_of(Exhibit)
   end
+
+  it "gets name" do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    expect(exhibit.name).to eq("Gems and Minerals")
+  end
+
+  it "gets cost" do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    expect(exhibit.cost).to eq(0)
+  end
+
 end
