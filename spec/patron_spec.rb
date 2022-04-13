f5981ed42c578f2f92db73fc19ad43ac327f936f
Stephen Wilkens <stephenwilkens@gmail.com>
2022-04-13 09:33:51 -0600

    Fix:adjusted name of Patron object to be uniform throughout tests

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -1,20 +1,20 @@
 require './lib/patron'
 
 describe Patron do
-  let(:patron){Patron.new("Bob", 20)}
+  let(:patron_1){Patron.new("Bob", 20)}
 
   it "exists" do
-    expect(patron).to be_a(Patron)
+    expect(patron_1).to be_a(Patron)
   end
 
   it "has readable attributes" do
-    expect(patron.name).to eq("Bob")
-    expect(patron.spending_money).to eq(20)
+    expect(patron_1.name).to eq("Bob")
+    expect(patron_1.spending_money).to eq(20)
   end
 
   it "has interests" do
     patron_1.add_interest("Dead Sea Scrolls")
     patron_1.add_interest("Gems and Minerals")
-    expect(patron.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
+    expect(patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
   end
 end
