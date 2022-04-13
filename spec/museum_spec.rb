3092f385e645bdc20132d601048e7dfa14925c17
tjhaines-cap <tjhaines@eckerd.edu>
2022-04-13 09:24:12 -0600

    Feat: test readable attributes for museum

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -10,4 +10,9 @@ describe Museum do
   it 'exists' do
     expect(@dmns).to be_a(Museum)
   end
+
+  it 'had readable attributes' do
+    expect(@dmns.name).to eq("Denver Museum of Nature and Science")
+    expect(@dmns.exhibits).to eq([])
+  end
 end
