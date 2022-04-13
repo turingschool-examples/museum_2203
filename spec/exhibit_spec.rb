2eaecf7af445817acbe475d3e4dcc1b95c2de271
Deannah Burke <deannah@bierstadtlager.com>
2022-04-13 09:07:31 -0600

    Write exist and attribute tests for exhibit class

---
+++ spec/exhibit_spec.rb
@@ -0,0 +1,28 @@
+require './lib/exhibit'
+require './lib/patron'
+
+RSpec.describe Exhibit do
+  it "exists" do
+  exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+  expect(exhibit).to be_a(Exhibit)
+  end
+
+  it "has attributes" do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+    expect(exhibit.name).to eq("Gems and Minerals")
+    expect(exhibit.cost).to eq(0)
+  end
+
+
+
+
+
+
+
+
+
+
+
+end
