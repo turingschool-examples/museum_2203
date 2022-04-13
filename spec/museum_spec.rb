93bb93d70ae1b4b0a71c1274bcf9820a5f16c1e5
Luke Swenson <LukeSwenson95@outlook.com>
2022-04-13 09:38:04 -0600

    test: added new test to add exhibits

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -8,10 +8,24 @@ describe Museum do
 
     expect(dmns).to be_a(Museum)
   end
+
   it "has attributes" do
     dmns = Museum.new("Denver Museum of Nature and Science")
 
     expect(dmns.name).to eq("Denver Museum of Nature and Science")
     expect(dmns.exhibits).to eq([])
+  end
+
+  it "can add and store exhibits" do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
+
   end
 end
