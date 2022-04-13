d9df0d43af27c0e5bb8ee46aa4a828f4fb9d408b
Stephen Wilkens <stephenwilkens@gmail.com>
2022-04-13 09:52:04 -0600

    Test:add add_exhibits test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -4,6 +4,11 @@ require './lib/museum'
 
 describe Museum do
   let(:dmns){Museum.new("Denver Museum of Nature and Science")}
+  let(:gems_and_minerals){Exhibit.new({name: "Gems and Minerals", cost: 0})}
+  let(:dead_sea_scrolls){Exhibit.new({name: "Dead Sea Scrolls", cost: 10})}
+  let(:imax){Exhibit.new({name: "IMAX",cost: 15})}
+
+
 
   it 'exists' do
     expect(dmns).to be_a(Museum)
@@ -12,5 +17,12 @@ describe Museum do
   it 'has readable attributes' do
     expect(dmns.name).to eq("Denver Museum of Nature and Science")
     expect(dmns.exhibits).to eq([])
+  end
+
+  it "can add exhibits" do
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
   end
 end
