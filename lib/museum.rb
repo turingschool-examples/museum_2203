9bd1b083fffb52c385af13a5fe925b75d94700bc
Sage Skaff <skaffsage@gmail.com>
2022-04-13 09:06:04 -0700

    Add patrons attribute

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,10 +1,12 @@
 class Museum
   attr_reader :name,
-              :exhibits
+              :exhibits,
+              :patrons
 
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(exhibit)
