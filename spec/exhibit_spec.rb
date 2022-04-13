689bcdddb75c5f0730500233aa48888d6f9786e8
Sarah Hall <swatson1994@yahoo.com>
2022-04-13 11:06:34 -0400

    "Test: Add first test for exhibit"

---
+++ spec/exhibit_spec.rb
@@ -0,0 +1,13 @@
+require 'rspec'
+require 'pry'
+require './lib/exhibit'
+describe Exhibit do
+  context 'iteration 1' do
+    before(:each) do
+      exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    end
+    it 'exists' do
+      expect(exhibit).to be_an_instance_of(Exhibit)
+    end
+  end
+end
