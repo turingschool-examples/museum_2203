f8975972e226f5cadfa0a7a4b55a3b48cbf49c47
Lucas Colwell <colwellc37@gmail.com>
2022-04-13 09:10:12 -0600

    Test existance

---
+++ spec/exhibit_spec.rb
@@ -0,0 +1,13 @@
+require 'rspec'
+require './lib/exhibit'
+
+RSpec.describe Exhibit do
+  before :each do
+    @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+  end
+
+  it "exists" do
+    expect(@exhibit).to be_a(Exhibit)
+  end
+
+end
