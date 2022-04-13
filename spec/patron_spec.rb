c80dedd25de764e4fbfc82827415c4f1d7de70f1
Zachary Hazelwood <zhaze001@gmail.com>
2022-04-13 09:13:59 -0600

    Add Patron attributes test

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -8,5 +8,11 @@ RSpec.describe do
     expect(patron_1).to be_a(Patron)
   end
 
+  it "has attributes" do
+    patron_1 = Patron.new("Bob", 20)
 
+    expect(patron_1.name).to eq("Bob")
+    expect(patron_1.spending_money).to eq(20)
+    expect(patron_1.interests).to eq([])
+  end
 end
