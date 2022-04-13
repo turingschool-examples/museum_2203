d898f61edf6bb2f57def17ddb55cd98311669163
Lucas Colwell <colwellc37@gmail.com>
2022-04-13 09:14:46 -0600

    Write tests for attributes

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -10,4 +10,9 @@ RSpec.describe Patron do
     expect(@patron_1).to be_a(Patron)
   end
 
+  it "has attributes" do
+    expect(@patron_1.name).to eq("Bob")
+    expect(@patron_1.spending_money).to eq(20)
+  end
+
 end
