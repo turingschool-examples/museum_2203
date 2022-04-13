615b1c2eb7860c3dbee197f7d46fc4f69126df54
Luke Swenson <LukeSwenson95@outlook.com>
2022-04-13 10:53:48 -0600

    feat: working on patron by exhibit interest code

--- lib/museum.rb
+++ lib/museum.rb
@@ -22,6 +22,24 @@ class Museum
   end
 
   def admit(patron)
-      @patrons << patron 
+      @patrons << patron
+  end
+
+  def patrons_by_exhibit_interest
+    exhibit_interest = {}
+    @patrons.each do |patron|
+      if recommend_exhibits(patron) == @exhibits[0]
+        exhibit_interest[@exhibits[0]] = [patron]
+      elsif recommend_exhibits(patron) == @exhibits[1]
+        exhibit_interest[@exhibits[1]] = [patron]
+      elsif recommend_exhibits(patron) == @exhibits[2]
+        exhibit_interest[@exhibits[2]] = [patron]
+      else
+        exhibit_interest[@exhibits[0]] = []
+        exhibit_interest[@exhibits[1]] = []
+        exhibit_interest[@exhibits[2]] = []
+  
+    end
+    end
   end
 end
