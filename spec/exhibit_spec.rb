f04b8a23a2ea2a4091f14d633d270d79b4282b60
Shirley DeCesari <shirley.decesari@gmail.com>
2022-04-13 08:09:28 -0700

    Initial commit

---
+++ spec/exhibit_spec.rb
@@ -0,0 +1,14 @@
+require './lib/exhibit'
+require './lib/patron'
+
+RSpec.describe Exhibit do
+  describe 'iteration I' do
+    it 'exists and has attributes' do
+      exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+      expect(exhibit).to be_a Exhibit
+      expect(exhibit.name).to eq "Gems and Minerals"
+      expect(exhibit.cost).to eq 0
+    end
+  end
+end
