9246c615e6d2e9fadcad8ef4f94e8298b459054a
Stephen Wilkens <stephenwilkens@gmail.com>
2022-04-13 09:57:58 -0600

    Test:add recommend exhibits test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -7,7 +7,8 @@ describe Museum do
   let(:gems_and_minerals){Exhibit.new({name: "Gems and Minerals", cost: 0})}
   let(:dead_sea_scrolls){Exhibit.new({name: "Dead Sea Scrolls", cost: 10})}
   let(:imax){Exhibit.new({name: "IMAX",cost: 15})}
-
+  let(:patron_1){Patron.new("Bob", 20)}
+  let(:patron_2){Patron.new("Sally", 20)}
 
 
   it 'exists' do
@@ -24,5 +25,13 @@ describe Museum do
     dmns.add_exhibit(dead_sea_scrolls)
     dmns.add_exhibit(imax)
     expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
+  end
+
+  it "can recommend exhibits" do
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    patron_2.add_interest("IMAX")
+    expect(dmns.recommend_exhibits(patron_1)).to eq([dead_sea_scrolls, gems_and_minerals])
+    expect(dmns.recommend_exhibits(patron_2)).to eq([imax])
   end
 end
