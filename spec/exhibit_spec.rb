a1a62531d4e519ca8d4124eeeaa5152ea7a82e6c
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-04-13 08:10:50 -0700

    test: exhibit spec tests

---
+++ spec/exhibit_spec.rb
@@ -0,0 +1,17 @@
+require './lib/exhibit'
+require './lib/patron'
+
+RSpec.describe Exhibit do
+  before :each do
+    @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+  end
+
+  it 'exists' do
+    expect(@exhibit).to be_a(Exhibit)
+  end
+
+  it 'has attributes' do
+    expect(@exhibit.name).to eq ("Gems and Minerals")
+    expect(@exhibit.cost).to eq 0
+  end
+end
