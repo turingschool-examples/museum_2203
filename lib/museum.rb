29ac7df195db6e666f1f113f4cf7e99a64b059ba
Colin Reinhart <colinbreinhartgmail.com>
2022-04-13 11:01:50 -0600

    Final Commit

--- lib/museum.rb
+++ lib/museum.rb
@@ -11,4 +11,13 @@ class Museum
     @exhibits << exhibit
   end
 
+  def recommend_exhibits(patron)
+    recommended_exhibits = []
+    patron.interests.each do |interest|
+      @exhibits.each do |exhibit|
+        recommended_exhibits << exhibit if exhibit.exhibit_hash[:name] == interest
+      end
+    end
+    recommended_exhibits
+  end
 end
--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -10,6 +10,14 @@ describe Museum do
     @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
     @imax = Exhibit.new({name: "IMAX",cost: 15})
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    @patron_1 = Patron.new("Bob", 20)
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_2 = Patron.new("Sally", 20)
+    @patron_2.add_interest("IMAX")
   end
 
   it 'exists' do
@@ -20,25 +28,20 @@ describe Museum do
     expect(@dmns.name).to eq("Denver Museum of Nature and Science")
   end
 
-  it 'has no exhibits' do
+  xit 'has no exhibits' do
     expect(@dmns.exhibits).to eq([])
   end
 
   it 'can add exhibits' do
-    @dmns.add_exhibit(@gems_and_minerals)
-    @dmns.add_exhibit(@dead_sea_scrolls)
-    @dmns.add_exhibit(@imax)
-# binding.pry
     expect(@dmns.exhibits).to eq([@gems_and_minerals, @dead_sea_scrolls, @imax])
   end
 
-  it 'can recomend exhibits based on interests' do
-    @patron_1 = Patron.new("Bob", 20)
-    @patron_1.add_interest("Dead Sea Scrolls")
-    @patron_1.add_interest("Gems and Minerals")
-    @patron_2 = Patron.new("Sally", 20)
-    @patron_2.add_interest("IMAX")
+  it 'can recommend exhibits based on interests' do
+    expect(@dmns.recommend_exhibits(@patron_1)).to eq([@dead_sea_scrolls, @gems_and_minerals])
+    expect(@dmns.recommend_exhibits(@patron2)).to eq([@imax])
+  end
 
-    expect(@dmns.recommend_exhibits).to eq()
+  it 'has patron by exhibit interest hash' do
+
   end
 end
