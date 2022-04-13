ce976cdea71996e3f82d41db79a20102e69a51b9
Sarah Hall <swatson1994@yahoo.com>
2022-04-13 11:38:28 -0400

    "Test: Add recommendation tests"

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -8,6 +8,11 @@ describe Museum do
       @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
       @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
       @imax = Exhibit.new({name: "IMAX",cost: 15})
+      @patron_1 = Patron.new("Bob", 20)
+      @patron_1.add_interest("Dead Sea Scrolls")
+      @patron_1.add_interest("Gems and Minerals")
+      @patron_2 = Patron.new("Sally", 20)
+      @patron_2.add_interest("IMAX")
     end
     it 'exists' do
       expect(@dmns).to be_an_instance_of(Museum)
@@ -23,6 +28,10 @@ describe Museum do
       @dmns.add_exhibit(@dead_sea_scrolls)
       @dmns.add_exhibit(@imax)
       expect(@dmns.exhibits).to eq([@gems_and_minerals, @dead_sea_scrolls, @imax])
+    end
+    it 'can recommend exhibits based on patron interests' do
+      expect(@dmns.recommend_exhibit(@patron_1)).to eq([@dead_sea_scrolls, @gems_and_minerals])
+      expect(@dmns.recommend_exhibit(@patron_)).to eq([@imax])
     end
   end
 end
