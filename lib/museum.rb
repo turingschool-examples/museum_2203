ab95b665d57b78ba59025adb614a0ff40462e17a
Eric Espindola <eric_e_2012@hotmail.com>
2022-04-13 10:01:21 -0700

    add iteration 3 methods

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,10 +1,11 @@
 require 'pry'
 class Museum
-  attr_reader :name, :exhibits
+  attr_reader :name, :exhibits, :patrons
 
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(exhibit)
@@ -19,5 +20,32 @@ class Museum
       end
     end
     recommended_exhibits
+  end
+
+  def admit(patron)
+    @patrons << patron
+  end
+
+  def patrons_by_exhibit_interest
+    interest_hash = {}
+    @exhibits.each do |exhibit|
+      interest_array = []
+      @patrons.each do |patron|
+        interest_array << patron if patron.interests.include?(exhibit.name)
+        interest_hash[exhibit] = interest_array
+      end
+    end
+    interest_hash
+  end
+
+  def ticket_lottery_contestants(exhibit)
+    interst_list = patrons_by_exhibit_interest
+    interst_list[exhibit]
+  end
+
+  def draw_lottery_winner(exhibit)
+    ticket_list = ticket_lottery_contestants(exhibit)
+    random_draw_number = rand(ticket_list.length -1)
+    ticket_list[random_draw_number]
   end
 end
--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -4,42 +4,97 @@ require './lib/patron'
 require './lib/museum'
 
 describe Museum do
+  before :each do
+    @dmns = Museum.new("Denver Museum of Nature and Science")
+    @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @imax = Exhibit.new({name: "IMAX",cost: 15})
+    @patron_1 = Patron.new("Bob", 20)
+    @patron_2 = Patron.new("Sally", 20)
+    @patron_3 = Patron.new("Johnny", 5)
+  end
+
   it "exists" do
-    dmns = Museum.new("Denver Museum of Nature and Science")
-    expect(dmns).to be_an_instance_of(Museum)
+    expect(@dmns).to be_an_instance_of(Museum)
   end
 
   it "has name" do
-    dmns = Museum.new("Denver Museum of Nature and Science")
-    expect(dmns.name).to eq("Denver Museum of Nature and Science")
+    expect(@dmns.name).to eq("Denver Museum of Nature and Science")
   end
 
   it "can add exhibits" do
-    dmns = Museum.new("Denver Museum of Nature and Science")
-    expect(dmns.exhibits).to eq([])
-    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
-    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
-    imax = Exhibit.new({name: "IMAX",cost: 15})
-    dmns.add_exhibit(gems_and_minerals)
-    dmns.add_exhibit(dead_sea_scrolls)
-    dmns.add_exhibit(imax)
-    expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
+    expect(@dmns.exhibits).to eq([])
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    expect(@dmns.exhibits).to eq([@gems_and_minerals, @dead_sea_scrolls, @imax])
   end
 
   it "recommends exhibits" do
-    dmns = Museum.new("Denver Museum of Nature and Science")
-    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
-    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
-    imax = Exhibit.new({name: "IMAX",cost: 15})
-    dmns.add_exhibit(gems_and_minerals)
-    dmns.add_exhibit(dead_sea_scrolls)
-    dmns.add_exhibit(imax)
-    patron_1 = Patron.new("Bob", 20)
-    patron_1.add_interest("Dead Sea Scrolls")
-    patron_1.add_interest("Gems and Minerals")
-    patron_2 = Patron.new("Sally", 20)
-    patron_2.add_interest("IMAX")
-    expect(dmns.recommend_exhibits(patron_1)).to eq([dead_sea_scrolls, gems_and_minerals])
-    expect(dmns.recommend_exhibits(patron_2)).to eq([imax])
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_2.add_interest("IMAX")
+    expect(@dmns.recommend_exhibits(@patron_1)).to eq([@dead_sea_scrolls, @gems_and_minerals])
+    expect(@dmns.recommend_exhibits(@patron_2)).to eq([@imax])
   end
+
+  it "museum admits patrons" do
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_2.add_interest("IMAX")
+    expect(@dmns.recommend_exhibits(@patron_1)).to eq([@dead_sea_scrolls, @gems_and_minerals])
+    expect(@dmns.recommend_exhibits(@patron_2)).to eq([@imax])
+  end
+
+  it "museum admits patrons" do
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_2.add_interest("IMAX")
+    @patron_3.add_interest("Dead Sea Scrolls")
+    expect(@dmns.patrons).to eq([])
+    @dmns.admit(@patron_1)
+    @dmns.admit(@patron_2)
+    @dmns.admit(@patron_3)
+    expect(@dmns.patrons).to eq([@patron_1,@patron_2,@patron_3])
+  end
+
+  it "museum has patrons by interest in exhibits" do
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_2.add_interest("IMAX")
+    @patron_3.add_interest("Dead Sea Scrolls")
+    expect(@dmns.patrons).to eq([])
+    @dmns.admit(@patron_1)
+    @dmns.admit(@patron_2)
+    @dmns.admit(@patron_3)
+    expect(@dmns.patrons_by_exhibit_interest.class).to eq(Hash)
+  end
+
+  it "gets lottery contestants" do
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_2.add_interest("IMAX")
+    @patron_3.add_interest("Dead Sea Scrolls")
+    expect(@dmns.patrons).to eq([])
+    @dmns.admit(@patron_1)
+    @dmns.admit(@patron_2)
+    @dmns.admit(@patron_3)
+    expect(@dmns.ticket_lottery_contestants(@dead_sea_scrolls)).to eq([@patron_1, @patron_3])
+  end
+
 end
