cf5ce25203c0c4cdaf87aa435a2df782ebecb494
Sage Skaff <skaffsage@gmail.com>
2022-04-13 08:12:53 -0700

    Update tests

---
+++ spec/patron_spec.rb
@@ -0,0 +1,27 @@
+require "rspec"
+require "./lib/patron"
+
+RSpec.describe Patron do
+  before(:each) do
+    @patron_1 = Patron.new("Bob", 20)
+  end
+
+  it "exists" do
+    expect(@patron_1).to be_a(Patron)
+  end
+
+  it "has readable attributes" do
+    expect(@patron_1.name).to eq("Bob")
+    expect(@patron_1.spending_money).to eq(20)
+  end
+
+  it "has no interests by default" do
+    expect(@patron_1.interests).to eq([])
+  end
+
+  it "can add interests" do
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+    expect(@patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
+  end
+end
