233414512bafa8238e4bb8b0bf13c908b2d41dff
Wesley Garcia <wes@alltradestech.com>
2022-04-13 08:40:13 -0700

    Test: Add test to check if Museum instance can recommend exhibits

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -23,6 +23,24 @@ RSpec.describe Museum do
     dmns.add_exhibit(gems_and_minerals)
     dmns.add_exhibit(dead_sea_scrolls)
     dmns.add_exhibit(imax)
+
     expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
+  end
+
+  it "can recommend exhibits" do
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX", cost: 15})
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("IMAX")
+
+    expect(dmns.recommend_exhibits(patron_1)).to eq([gems_and_minerals, dead_sea_scrolls])
+    expect(dmns.recommend_exhibits(patron_2)).to eq([imax])
   end
 end
