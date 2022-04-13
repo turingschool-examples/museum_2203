41c9b30f95fe9fa70ec4663f2cff44836b2f34bc
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-04-13 09:10:40 -0600

    Feat: finish iteration 1 with passing tests

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -11,4 +11,13 @@ RSpec.describe Patron do
         expect(patron_1.spending_money).to eq(20)
         expect(patron_1.interests).to eq([])
     end
+
+    it 'can have interests' do 
+        patron_1 = Patron.new("Bob", 20)
+
+        patron_1.add_interest("Dead Sea Scrolls")
+        patron_1.add_interest("Gems and Minerals")
+
+        expect(patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
+    end
 end
