8b7d8a6da00892930feeb52de7b32c9909cc7156
Sage Skaff <skaffsage@gmail.com>
2022-04-13 09:40:20 -0700

    Add patrons by exhibit interest method

--- lib/museum.rb
+++ lib/museum.rb
@@ -22,4 +22,17 @@ class Museum
   def admit(patron)
     @patrons << patron
   end
+
+  def patrons_by_exhibit_interest
+    pbei = Hash.new
+    @exhibits.each do |exhibit|
+      pbei[exhibit] = []
+    end
+    pbei.each do |exhibit, patron_arr|
+      @patrons.each do |patron|
+        patron_arr << patron if patron.interests.include?(exhibit.name)
+      end.compact
+    end
+    pbei
+  end
 end
--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -73,7 +73,6 @@ RSpec.describe Museum do
     @dmns.admit(patron_1)
     @dmns.admit(patron_2)
     @dmns.admit(patron_3)
-    require "pry"; binding.pry
     expect(@dmns.patrons_by_exhibit_interest).to eq({
       @gems_and_minerals => [patron_1],
       @dead_sea_scrolls => [patron_2, patron_3],
