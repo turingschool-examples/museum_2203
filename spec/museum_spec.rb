082e65fb7c2dbfc6ab3197de16a78855deecbf90
Stephen Wilkens <stephenwilkens@gmail.com>
2022-04-13 10:27:56 -0600

    Test:add patrons by interest test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -53,4 +53,20 @@ describe Museum do
     dmns.admit(patron_3)
     expect(dmns.patrons).to eq([patron_1, patron_2, patron_3])
   end
+
+  it "can give patrons by interest" do
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_2.add_interest("Dead Sea Scrolls")
+    patron_3.add_interest("Dead Sea Scrolls")
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+    expect(dmns.patrons_by_exhibit_interest).to eq(gems_and_minerals => [patron_1],
+    dead_sea_scrolls => [patron_1, patron_2, patron_3], 
+    imax => [])
+  end
 end
