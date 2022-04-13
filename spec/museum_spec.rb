92bedad5fb242c3275a3b191fda627c4c20fc4ed
Wesley Garcia <wes@alltradestech.com>
2022-04-13 08:34:51 -0700

    Test: Add test to check if Museum instance can add exhibits

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -15,4 +15,14 @@ RSpec.describe Museum do
     expect(dmns.name).to eq("Denver Museum of Nature and Science")
     expect(dmns.exhibits).to eq([])
   end
+
+  it "can add exhibits" do
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX", cost: 15})
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
+  end
 end
