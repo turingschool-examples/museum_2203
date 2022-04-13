217f173871574646033784648ec3820912ebfd25
tjhaines-cap <tjhaines@eckerd.edu>
2022-04-13 09:16:20 -0600

    Feat: add attr reader to patron class

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -14,7 +14,7 @@ describe Patron do
   it 'has readable attributes' do
     expect(@patron_1.name).to eq("Bob")
     expect(@patron_1.spending_money).to eq(20)
-    expect(@patron_1.interests)to eq([])
+    expect(@patron_1.interests).to eq([])
   end
 
 end
