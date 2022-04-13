353fbc282b7cfd6205201efaf0cfb25d32d78ef6
Luke Swenson <LukeSwenson95@outlook.com>
2022-04-13 09:59:39 -0600

    test/feat: rewrote wrong test and added recommend_exhibits method

--- lib/museum.rb
+++ lib/museum.rb
@@ -9,4 +9,13 @@ class Museum
   def add_exhibit(exhibit)
     @exhibits << exhibit
   end
+
+  def recommend_exhibits(patron)
+    recommended_exhibits = []
+    @exhibits.find_all do |exhibit|
+      if patron.interests.include?(exhibit.name)
+        recommended_exhibits << exhibit
+      end
+    end
+  end
 end
--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -43,7 +43,7 @@ describe Museum do
     patron_1.add_interest("Dead Sea Scrolls")
     patron_1.add_interest("Gems and Minerals")
     patron_2.add_interest("IMAX")
-    expect(dmns.recommend_exhibits(patron_1)).to eq([dead_sea_scrolls, gems_and_minerals])
+    expect(dmns.recommend_exhibits(patron_1)).to eq([gems_and_minerals, dead_sea_scrolls])
     expect(dmns.recommend_exhibits(patron_2)).to eq([imax])
 
   end
