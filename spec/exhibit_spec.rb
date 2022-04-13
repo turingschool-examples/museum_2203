a10674e6d9da67b236ef3d703a8484d0ba8a7c00
Sarah Hall <swatson1994@yahoo.com>
2022-04-13 11:12:23 -0400

    "test: Fix missing @"

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -4,10 +4,10 @@ require './lib/exhibit'
 describe Exhibit do
   context 'iteration 1' do
     before(:each) do
-      exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+      @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
     end
     it 'exists' do
-      expect(exhibit).to be_an_instance_of(Exhibit)
+      expect(@exhibit).to be_an_instance_of(Exhibit)
     end
   end
 end
