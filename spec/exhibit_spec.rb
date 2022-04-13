4dad1d6892601787a81815b519a125e7f6219f31
Jim Riddle <jimriddle0@gmail.com>
2022-04-13 10:12:39 -0500

    clean up some tests, add test for interests method on patron

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -1,5 +1,7 @@
 require './lib/exhibit'
 require './lib/museum'
+require './lib/patron'
+
 
 
 
@@ -8,7 +10,7 @@ RSpec.describe Exhibit do
 
   it 'exists' do
     exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
-    expect(exhibit).to be(Exhibit)
+    expect(exhibit).to be_an_instance_of(Exhibit)
   end
 
   it 'has readable attributes' do
--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -1,5 +1,6 @@
 require './lib/exhibit'
 require './lib/museum'
+require './lib/patron'
 
 
 
@@ -8,7 +9,7 @@ RSpec.describe Patron do
 
   it 'exists' do
     patron_1 = Patron.new("Bob", 20)
-    expect(patron_1).to be(Patron)
+    expect(patron_1).to be_an_instance_of(Patron)
   end
 
   it 'has readable attributes' do
@@ -16,7 +17,14 @@ RSpec.describe Patron do
     expect(patron_1.name).to eq("Bob")
     expect(patron_1.spending_money).to eq(20)
     expect(patron_1.interests).to eq([])
+  end
 
+  it 'can add interests' do
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+
+    expect(patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
   end
 
 end
