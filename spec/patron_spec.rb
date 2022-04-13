6dd5602cef7389160ab4f5706894fded3b7cf19b
Andrew King <dev.andrewking@gmail.com>
2022-04-13 10:39:39 -0500

    add interests test to patron

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -10,6 +10,15 @@ RSpec.describe Patron do
     patron_1 = Patron.new("Bob", 20)
     expect(patron_1.name).to eq("Bob")
     expect(patron_1.spending_money).to eq(20)
+    expect(patron_1.interests).to eq([])
+
+  end
+
+  it 'can add interests' do
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    expect(patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
 
   end
 
