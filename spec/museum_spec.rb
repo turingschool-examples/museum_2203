f3b617470cc27893675d3847b0bf30e2212248d2
Andrew King <dev.andrewking@gmail.com>
2022-04-13 11:12:44 -0500

    add admit test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe Museum do
     dmns = Museum.new("Denver Museum of Nature and Science")
     expect(dmns.name).to eq("Denver Museum of Nature and Science")
     expect(dmns.exhibits).to eq([])
+    expect(dmns.patrons).to eq([])
   end
 
   it 'can add exhibits' do
@@ -40,6 +41,27 @@ RSpec.describe Museum do
     patron_2.add_interest("IMAX")
     expect(dmns.recommend_exhibits(patron_1)).to eq([dead_sea_scrolls, gems_and_minerals])
     expect(dmns.recommend_exhibits(patron_2)).to eq([imax])
+  end
+
+  it 'can have patrons' do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("IMAX")
+    patron_3 = Patron.new("Johnny", 5)
+    patron_3.add_interest("Dead Sea Scrolls")
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+    expect(dmns.patrons).to eq([patron_1, patron_2, patron_3])
   end
 
 
