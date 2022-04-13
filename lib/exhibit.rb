e9bd691b1d1117e25a4bdb20b2ebf3fcbeafede3
Eric Espindola <eric_e_2012@hotmail.com>
2022-04-13 08:16:01 -0700

    add methods to exhibit class. passes all tests

---
+++ lib/exhibit.rb
@@ -0,0 +1,15 @@
+class Exhibit
+  attr_reader :exhibit_hash
+
+  def initialize(exhibit_hash)
+    @exhibit_hash = exhibit_hash
+  end
+
+  def name
+    @exhibit_hash[:name]
+  end
+
+  def cost
+    @exhibit_hash[:cost]
+  end
+end
