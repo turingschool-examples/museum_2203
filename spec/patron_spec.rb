64fecbbf0639ff5dd80973a84ba768b5baeec5f1
Stirling Hostetter <stirhoss@icloud.com>
2022-04-13 10:24:10 -0500

    Add test for interests

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -17,4 +17,10 @@ describe 'patron' do
     expect(patron_1.name).to eq "Bob"
     expect(patron_1.spending_money).to eq 20
   end
+
+  it 'has no interests initially' do
+    patron_1 = Patron.new("Bob", 20)
+
+    expect(patron_1.interests).to eq []
+  end
 end
