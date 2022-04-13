4bdcd9edaddbedbb224f730de3977c0e250c202e
Wesley Garcia <wes@alltradestech.com>
2022-04-13 09:23:03 -0700

    Test add: check eligibility for exhibit lottery

--- lib/museum.rb
+++ lib/museum.rb
@@ -24,4 +24,16 @@ class Museum
   def admit(patron)
     @patrons << patron
   end
+
+  def patrons_by_exhibit_interest
+    exh_with_int_patrons = {}
+    @exhibits.each do |exhibit|
+      interested_patrons = []
+      @patrons.each do |patron|
+        interested_patrons << patron if patron.interests.include?(exhibit.name)
+      end
+      exh_with_int_patrons[exhibit] = interested_patrons
+    end
+    exh_with_int_patrons
+  end
 end
