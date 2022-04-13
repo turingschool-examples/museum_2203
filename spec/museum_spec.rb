b84487b07ba4c6b67e9eb1aae8fe67c670b5572d
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-04-13 09:13:23 -0600

    Feat: add first test for museum

---
+++ spec/museum_spec.rb
@@ -0,0 +1,14 @@
+require './lib/museum'
+require './lib/patron'
+require './lib/exhibit'
+
+RSpec.describe Museum do 
+
+    it 'exists and has attributes' do
+        dmns = Museum.new("Denver Museum of Nature and Science")
+
+        expect(dmns).to be_an_instance_of(Museum)
+        expect(dmns.name).to eq("Denver Museum of Nature and Science")
+        expect(dmns.exhibits).to eq([])
+    end
+end
