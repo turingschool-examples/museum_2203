d2ce4e0a6d7243bdbd7d182c64826df95051c8eb
Thiago Silveira <thiago1992@yahoo.com>
2022-04-13 09:12:57 -0600

    Initial Commit

---
+++ lib/exhibit.rb
@@ -0,0 +1,17 @@
+class Exhibit
+
+  def initialize(attribute)
+    @name = attribute[:name]
+    @cost = attribute[:cost]
+  end
+
+  def name
+    @name
+  end
+
+  def cost
+    @cost
+  end
+
+  
+end
