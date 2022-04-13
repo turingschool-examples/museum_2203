98fa925f9a5d0cbeaf91d1c0f5412fd6f825eafa
“Brad <hutch13531@gmail.com>
2022-04-13 11:50:42 -0400

    Test: has readable attributes

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -1,11 +1,18 @@
 require './lib/exhibit'
 
 RSpec.describe Exhibit do
-  
+
   it 'exist' do
     exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
   expect(exhibit).to be_an_instance_of(Exhibit)
   end
+
+  it 'has readable attributes' do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+  expect(exhibit.name).to eq("Gems and Minerals")
+  expect(exhibit.cost).to eq(0)
+  end
+
 
 
 end
