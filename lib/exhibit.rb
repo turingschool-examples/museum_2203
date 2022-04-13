2f0e0f0f6ebc2ba853bbda57cc5ebb5500777041
tjhaines-cap <tjhaines@eckerd.edu>
2022-04-13 09:08:51 -0600

    Feat: add attr reader to exhibit

--- lib/exhibit.rb
+++ lib/exhibit.rb
@@ -1,5 +1,7 @@
 class Exhibit
 
+  attr_reader :name, :cost
+  
   def initialize(info_hash)
     @name = info_hash[:name]
     @cost = info_hash[:cost]
--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -10,4 +10,9 @@ describe Exhibit do
   it 'exists' do
     expect(@exhibit).to be_a(Exhibit)
   end
+
+  it 'has readable attributes' do
+    expect(@exhibit.name).to eq("Gems and Minerals")
+    expect(@exhibit.cost).to eq(0)
+  end
 end
