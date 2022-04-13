21c974b235eb02a66d401d08b5aab81ef8a254e6
“Brad <hutch13531@gmail.com>
2022-04-13 12:08:43 -0400

    Test: Museum-has readable attributes

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -6,6 +6,14 @@ require './lib/exhibit'
 RSpec.describe Museum do
   it 'exist' do
     dmns = Museum.new("Denver Museum of Nature and Science")
+  expect(dmns).to be_an_instance_of(Museum)
   end
+
+  it 'has readable attributes' do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+  expect(dmns.name).to eq("Denver Museum of Nature and Science")
+  expect(dmns.exhibits).to eq([])
+  end
+
 
 end
