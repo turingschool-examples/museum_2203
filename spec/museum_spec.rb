541dd89ed4b25b14f30be6859e87a8961d7b43b8
Lucas Colwell <colwellc37@gmail.com>
2022-04-13 09:37:49 -0600

    Initial commit plus structure

---
+++ spec/museum_spec.rb
@@ -0,0 +1,18 @@
+require 'rspec'
+require './lib/patron'
+require './lib/exhibit'
+
+RSpec.describe Museum do
+  before :each do
+
+  end
+
+  it "exists" do
+    expect(@).to be_a(Museum)
+  end
+
+  it "has attributes" do
+
+  end
+
+end
