453f5af7228666ea0180faa909879db7d1898060
Clay Ash <claycourtneyash@gmail.com>
2022-04-13 10:05:13 -0600

    Test: patrons by interest test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -61,7 +61,29 @@ RSpec.describe Museum do
     expect(dmns.patrons).to eq([patron_1, patron_2, patron_3])
   end
 
-  it 'can list patrons by interest' do
+  it 'can list patrons interested in exhibit' do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(imax)
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("Dead Sea Scrolls")
+    patron_3 = Patron.new("Johnny", 5)
+    patron_3.add_interest("Dead Sea Scrolls")
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+    expect(dmns.patrons_by_interests(dead_sea_scrolls)).to eq([patron_1, patron_2, patron_3])
+  end
+
+
+  xit 'can list patrons by interest' do
     dmns = Museum.new("Denver Museum of Nature and Science")
     gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
