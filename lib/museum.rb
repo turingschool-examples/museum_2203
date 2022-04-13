d291cb54f6bd6dcf74f16205d7a14442f09bd8b0
tjhaines-cap <tjhaines@eckerd.edu>
2022-04-13 09:39:05 -0600

    Feat: create recommend exhibits in museum class

--- lib/museum.rb
+++ lib/museum.rb
@@ -11,5 +11,14 @@ class Museum
   def add_exhibit(exhibit)
     @exhibits << exhibit
   end
-  
+
+  def recommend_exhibits(patron)
+    recmd_exhib = []
+    @exhibits.each do |exhibit|
+      if patron.interests.include?(exhibit.name)
+        recmd_exhib << exhibit
+      end
+    end
+    return recmd_exhib
+  end
 end
--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -40,8 +40,7 @@ describe Museum do
     @dmns.add_exhibit(@dead_sea_scrolls)
     @dmns.add_exhibit(@imax)
 
-
-    expect(@dmns.recommend_exhibits(@patron_1)).to eq([@dead_sea_scrolls, @gems_and_minerals])
+    expect(@dmns.recommend_exhibits(@patron_1)).to eq([@gems_and_minerals, @dead_sea_scrolls])
     expect(@dmns.recommend_exhibits(@patron_2)).to eq([@imax])
   end
 end
