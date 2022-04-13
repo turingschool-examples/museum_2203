66e293eb1ed718c8e01f0f4e8c55e7357258e4cd
Andrew King <dev.andrewking@gmail.com>
2022-04-13 10:35:47 -0500

    add exist and attribute test to patron

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -1,7 +1,18 @@
 require './lib/patron'
 RSpec.describe Patron do
 
+  it 'exists' do
+    patron_1 = Patron.new("Bob", 20)
+    expect(patron_1).to be_a(Person)
+  end
+
+  it 'has attributes' do
+    patron_1 = Patron.new("Bob", 20)
+    expect(patron_1.name).to eq("Bob")
+    expect(patron_1.spending_money).to eq(20)
+
+  end
 
 
-  
+
 end
