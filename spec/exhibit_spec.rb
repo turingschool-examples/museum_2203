82060cacbf8f320f1c6e1fb827da22db521e49ec
Sage Skaff <skaffsage@gmail.com>
2022-04-13 08:07:56 -0700

    Update tests

---
+++ spec/exhibit_spec.rb
@@ -0,0 +1,17 @@
+require "rspec"
+require "./lib/exhibit"
+
+RSpec.describe Exhibit do
+  before(:each) do
+    @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+  end
+
+  it "exists" do
+    expect(@exhibit).to be_a(Exhibit)
+  end
+
+  it "has readable attributes" do
+    expect(@exhibit.name).to eq("Gems and Minerals")
+    expect(@exhibit.cost).to eq(0)
+  end
+end
