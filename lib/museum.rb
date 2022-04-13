3c05d1900ea57d45e23def0030aaae1c30b91b46
Andrew King <dev.andrewking@gmail.com>
2022-04-13 11:08:23 -0500

    add recommend_exhibit method

--- lib/museum.rb
+++ lib/museum.rb
@@ -11,4 +11,12 @@ class Museum
     @exhibits << exhibit
   end
 
+  def recommend_exhibits(patron)
+    patron_recommendations = []
+    @exhibits.each do |exhibit|
+        patron_recommendations << exhibit if patron.interests.include?(exhibit.name)
+    end
+    patron_recommendations
+  end
+
 end
--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -12,10 +12,10 @@ RSpec.describe Museum do
     dmns = Museum.new("Denver Museum of Nature and Science")
     expect(dmns.name).to eq("Denver Museum of Nature and Science")
     expect(dmns.exhibits).to eq([])
-
   end
 
   it 'can add exhibits' do
+    dmns = Museum.new("Denver Museum of Nature and Science")
     gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
     imax = Exhibit.new({name: "IMAX",cost: 15})
@@ -23,16 +23,15 @@ RSpec.describe Museum do
     dmns.add_exhibit(dead_sea_scrolls)
     dmns.add_exhibit(imax)
     expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
-
   end
 
   it 'can recommend exhibits' do
     dmns = Museum.new("Denver Museum of Nature and Science")
-    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     imax = Exhibit.new({name: "IMAX",cost: 15})
-    dmns.add_exhibit(gems_and_minerals)
     dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(gems_and_minerals)
     dmns.add_exhibit(imax)
     patron_1 = Patron.new("Bob", 20)
     patron_1.add_interest("Dead Sea Scrolls")
@@ -41,8 +40,6 @@ RSpec.describe Museum do
     patron_2.add_interest("IMAX")
     expect(dmns.recommend_exhibits(patron_1)).to eq([dead_sea_scrolls, gems_and_minerals])
     expect(dmns.recommend_exhibits(patron_2)).to eq([imax])
-
-
   end
 
 
