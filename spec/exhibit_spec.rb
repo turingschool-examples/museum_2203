274ef1311d9b6c24929bc630a17245eec5332e83
Nick Jacobs <nickjacobsatc@gmail.com>
2022-04-13 10:27:51 -0500

    Test: exists & has readable attributes

---
+++ spec/exhibit_spec.rb
@@ -0,0 +1,17 @@
+require './lib/exhibit'
+require './lib/patron'
+
+RSpec.describe Exhibit do
+
+  it "exists" do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    expect(exhibit).to be_an_instance_of(Exhibit)
+  end
+
+  it "has readable attributes" do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    expect(exhibit.name).to eq("Gems and Minerals")
+    expect(exhibit.cost).to eq(0)
+  end
+
+end
