35a043fa20d30a7fe767d62bde882582e891a6d1
Bryce Wein <bryce.wein@gmail.com>
2022-04-13 11:43:13 -0400

    Test: add ticket lottery test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -85,4 +85,25 @@ describe Museum do
                                                      dead_sea_scrolls => [patron_1, patron_2, patron_3],
                                                      imax => [] })
   end
+
+  it 'can return an array of ticket lottery contestants' do
+    dmns = Museum.new('Denver Museum of Nature and Science')
+    gems_and_minerals = Exhibit.new({ name: 'Gems and Minerals', cost: 0 })
+    dead_sea_scrolls = Exhibit.new({ name: 'Dead Sea Scrolls', cost: 10 })
+    imax = Exhibit.new({ name: 'IMAX', cost: 15 })
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    patron_1 = Patron.new('Bob', 0)
+    patron_1.add_interest('Gems and Minerals')
+    patron_1.add_interest('Dead Sea Scrolls')
+    patron_2 = Patron.new('Sally', 20)
+    patron_2.add_interest('Dead Sea Scrolls')
+    patron_3 = Patron.new('Johnny', 5)
+    patron_3.add_interest('Dead Sea Scrolls')
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+    expect(dmns.ticket_lottery_contestants(dead_sea_scrolls)).to eq([patron_1, patron_3])
+  end
 end
