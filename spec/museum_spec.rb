35299432725df2552ff5c8bd6853e2f15fcbb98e
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-04-13 09:35:17 -0600

    Feat: add test for adding patrons it 3

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -47,4 +47,34 @@ RSpec.describe Museum do
         expect(dmns.recommend_exhibits(patron_1)).to eq([dead_sea_scrolls, gems_and_minerals])
         expect(dmns.recommend_exhibits(patron_2)).to eq([imax])
     end
+
+    it 'can admit patrons' do 
+        dmns = Museum.new("Denver Museum of Nature and Science")
+
+        gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+        dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+        imax = Exhibit.new({name: "IMAX",cost: 15})
+
+        dmns.add_exhibit(gems_and_minerals)
+        dmns.add_exhibit(dead_sea_scrolls)
+        dmns.add_exhibit(imax)
+
+        expect(dmns.patrons).to eq([])
+
+        patron_1 = Patron.new("Bob", 0)
+        patron_1.add_interest("Dead Sea Scrolls")
+        patron_1.add_interest("Gems and Minerals")
+
+        patron_2 = Patron.new("Sally", 20)
+        patron_2.add_interest("Dead Sea Scrolls")
+
+        patron_3 = Patron.new("Johnny", 5)
+        patron_3.add_interest("Dead Sea Scrolls")
+
+        dmns.admit(patron_1)
+        dmns.admit(patron_2)
+        dmns.admit(patron_3)
+
+        expect(dmns.patrons).to eq([patron_1, patron_2, patron_3])
+    end
 end
