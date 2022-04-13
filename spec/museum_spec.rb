334591658bec0dbcf2bb4b953aa1baf16604b59d
Lucas Colwell <colwellc37@gmail.com>
2022-04-13 10:04:10 -0600

    Write test for recommend_exhibits

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -9,6 +9,8 @@ RSpec.describe Museum do
     @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
     @imax = Exhibit.new({name: "IMAX",cost: 15})
+    @patron_1 = Patron.new("Bob", 20)
+    @patron_2 = Patron.new("Sally", 20)
   end
 
   it "exists" do
@@ -26,5 +28,14 @@ RSpec.describe Museum do
     @dmns.add_exhibit(@imax)
 
     expect(@dmns.exhibits).to eq([@gems_and_minerals, @dead_sea_scrolls, @imax])
+  end
+
+  it "can recommend exhibits" do
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_2.add_interest("IMAX")
+
+    expect(@dmns.recommend_exhibits(@patron_1)).to eq([@dead_sea_scrolls, @gems_and_minerals])
+    expect(@dmns.recommend_exhibits(@patron_2)).to eq([@imax])
   end
 end
