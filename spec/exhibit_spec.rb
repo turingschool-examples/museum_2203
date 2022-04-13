bd1def1c214afb2810ee9188117a56fd405aec64
Zachary Hazelwood <zhaze001@gmail.com>
2022-04-13 09:07:57 -0600

    Add attributes test

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -9,4 +9,11 @@ RSpec.describe do
     expect(exhibit).to be_a(Exhibit)
   end
 
+  it "has attributes" do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+    expect(exhibit.name).to eq("Gems and Minerals")
+    expect(exhibit.cost).to eq(0)
+  end
+
 end
