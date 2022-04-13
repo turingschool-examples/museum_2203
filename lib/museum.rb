9cdeeff964b092e1a0500ce9b75947eba4e7fff1
Sarah Hall <swatson1994@yahoo.com>
2022-04-13 11:34:33 -0400

    "Feat: Add museum class and make pass first round of tests"

---
+++ lib/museum.rb
@@ -0,0 +1,14 @@
+require './lib/patron'
+require './lib/exhibit'
+
+class Museum
+  attr_reader :name, :exhibits
+  def initialize(name)
+    @name = name
+    @exhibits = []
+  end
+
+  def add_exhibit(new_exhibit)
+    @exhibits << new_exhibit
+  end
+end
