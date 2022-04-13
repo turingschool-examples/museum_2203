10e74307cd2851d2fd5fe1ff79da31a3a47bbf69
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-04-13 12:54:03 -0400

    FINALLY iteration 3, yikes

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,10 +1,12 @@
 class Museum
   attr_reader :name,
-              :exhibits
+              :exhibits,
+              :patrons
 
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(exhibit)
@@ -12,11 +14,44 @@ class Museum
   end
 
   def recommend_exhibits(patron)
-    recommendation = @exhibits.select do |exhibit|
+    @exhibits.select do |exhibit|
       patron.interests.include?(exhibit.name)
     end
   end
 
+  def admit(patron)
+    @patrons << patron
+  end
 
+  def patrons_by_exhibit_interest
+    patrons_by_exhibit = {}
+    @exhibits.each do |exhibit|
+      if !patrons_by_exhibit.has_key?(exhibit)
+        patrons_by_exhibit[exhibit] =
+        @patrons.select do |patron|
+          patron.interests.include?(exhibit.name)
+        end
+      end
+    end
+    patrons_by_exhibit
+  end
+
+  def ticket_lottery_contestants(exhibit)
+    ## I need to look at all patrons interested in that exibit and check how much money they have
+    patrons_by_exhibit_interest[exhibit].select do |patron|
+      patron.spending_money < exhibit.cost
+    end
+  end
+
+  def draw_lottery_winner(exhibit)
+    # require "pry"; binding.pry
+    if ticket_lottery_contestants(exhibit) == []
+      nil
+    else
+    ticket_lottery_contestants(exhibit).sample.name
+    end
+  end
+
+  
 
 end
--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -50,4 +50,108 @@ RSpec.describe Museum do
     expect(dmns.recommend_exhibits(patron_2)).to eq([imax])
 
   end
+
+  it "can list admitted patrons" do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    expect(dmns.patrons).to eq([])
+
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("IMAX")
+    patron_3 = Patron.new("Johnny", 5)
+    patron_3.add_interest("Dead Sea Scrolls")
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+
+    expect(dmns.patrons).to eq([patron_1, patron_2, patron_3])
+
+  end
+
+  it "can list admitted patrons by exhibit interest" do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("Dead Sea Scrolls")
+    patron_3 = Patron.new("Johnny", 5)
+    patron_3.add_interest("Dead Sea Scrolls")
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+
+    expected = {
+      gems_and_minerals => [patron_1],
+      dead_sea_scrolls => [patron_1, patron_2, patron_3],
+      imax => []
+    }
+
+    expect(dmns.patrons_by_exhibit_interest).to eq(expected)
+  end
+
+  it 'can return an array of ticket lottery contestants' do
+      dmns = Museum.new('Denver Museum of Nature and Science')
+      gems_and_minerals = Exhibit.new({ name: 'Gems and Minerals', cost: 0 })
+      dead_sea_scrolls = Exhibit.new({ name: 'Dead Sea Scrolls', cost: 10 })
+      imax = Exhibit.new({ name: 'IMAX', cost: 15 })
+      dmns.add_exhibit(gems_and_minerals)
+      dmns.add_exhibit(dead_sea_scrolls)
+      dmns.add_exhibit(imax)
+
+      patron_1 = Patron.new('Bob', 0)
+      patron_1.add_interest('Gems and Minerals')
+      patron_1.add_interest('Dead Sea Scrolls')
+      patron_2 = Patron.new('Sally', 20)
+      patron_2.add_interest('Dead Sea Scrolls')
+      patron_3 = Patron.new('Johnny', 5)
+      patron_3.add_interest('Dead Sea Scrolls')
+
+      dmns.admit(patron_1)
+      dmns.admit(patron_2)
+      dmns.admit(patron_3)
+      expect(dmns.ticket_lottery_contestants(dead_sea_scrolls)).to eq([patron_1, patron_3])
+    end
+
+  it 'can pick a lottery winner' do
+      dmns = Museum.new('Denver Museum of Nature and Science')
+      gems_and_minerals = Exhibit.new({ name: 'Gems and Minerals', cost: 0 })
+      dead_sea_scrolls = Exhibit.new({ name: 'Dead Sea Scrolls', cost: 10 })
+      imax = Exhibit.new({ name: 'IMAX', cost: 15 })
+      dmns.add_exhibit(gems_and_minerals)
+      dmns.add_exhibit(dead_sea_scrolls)
+      dmns.add_exhibit(imax)
+
+      patron_1 = Patron.new('Bob', 0)
+      patron_1.add_interest('Gems and Minerals')
+      patron_1.add_interest('Dead Sea Scrolls')
+      patron_2 = Patron.new('Sally', 20)
+      patron_2.add_interest('Dead Sea Scrolls')
+      patron_3 = Patron.new('Johnny', 5)
+      patron_3.add_interest('Dead Sea Scrolls')
+
+      dmns.admit(patron_1)
+      dmns.admit(patron_2)
+      dmns.admit(patron_3)
+      expect(dmns.draw_lottery_winner(gems_and_minerals)).to eq(nil)
+      expect(dmns.draw_lottery_winner(dead_sea_scrolls)).to eq("Bob")
+      #crunched for time and don't know how to test this....soo just accepting that it passes sometimes depending who is picked LOL
+    end
+
 end
