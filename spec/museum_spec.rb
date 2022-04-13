2f16502b12568c252f70e21c629dc332b5c46909
Stephen Wilkens <stephenwilkens@gmail.com>
2022-04-13 10:18:21 -0600

    Test:add admit patrons test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -9,6 +9,7 @@ describe Museum do
   let(:imax){Exhibit.new({name: "IMAX",cost: 15})}
   let(:patron_1){Patron.new("Bob", 20)}
   let(:patron_2){Patron.new("Sally", 20)}
+  let(:patron_3){Patron.new("Johnny", 5)}
 
 
   it 'exists' do
@@ -36,5 +37,20 @@ describe Museum do
     patron_2.add_interest("IMAX")
     expect(dmns.recommend_exhibits(patron_1)).to eq([dead_sea_scrolls, gems_and_minerals])
     expect(dmns.recommend_exhibits(patron_2)).to eq([imax])
+  end
+
+  it "can admit patrons" do
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_2.add_interest("IMAX")
+    patron_3.add_interest("Dead Sea Scrolls")
+    expect(dmns.patrons).to eq([])
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+    expect(dmns.patrons).to eq([patron_1, patron_2, patron_3])
   end
 end
