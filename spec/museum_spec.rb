374831f57feeed53bc7265953a5686e8d3ab6897
Andrew King <dev.andrewking@gmail.com>
2022-04-13 11:41:11 -0500

    add draw lottery winner test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -111,6 +111,27 @@ RSpec.describe Museum do
     expect(dmns.ticket_lottery_contestants(dead_sea_scrolls)).to eq([patron_1, patron_3])
   end
 
+  it 'can return lottery winner' do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    patron_1 = Patron.new("Bob", 0)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("IMAX")
+    patron_3 = Patron.new("Johnny", 5)
+    patron_3.add_interest("Dead Sea Scrolls")
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+    expect(dmns.draw_lottery_winner(dead_sea_scrolls)).to eq(patron_1 || patron_3)
+  end
+
 
 
 end
