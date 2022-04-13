78e62a5cf4229cf5c33e69736e1711a924c5aa28
tjhaines-cap <tjhaines@eckerd.edu>
2022-04-13 09:17:37 -0600

    Feat: test add interests for patron

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -17,4 +17,10 @@ describe Patron do
     expect(@patron_1.interests).to eq([])
   end
 
+  it 'can add interests' do
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+    expect(@patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
+  end
+
 end
