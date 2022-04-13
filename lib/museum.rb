b60656d5699bb49aa9e5520f35cc93a48a4b8dee
Stirling Hostetter <stirhoss@icloud.com>
2022-04-13 11:18:03 -0500

    Add test for patrons_by_exhibit_interest

--- lib/museum.rb
+++ lib/museum.rb
@@ -3,14 +3,20 @@ require './lib/exhibit'
 require './lib/patron'
 
 class Museum
-  attr_reader :name, :exhibits
+  attr_reader :name, :exhibits, :patrons
+
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(exhibit)
     @exhibits << exhibit
+  end
+
+  def admit(patron)
+    @patrons << patron
   end
 
   def recommend_exhibits(patron)
--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -62,11 +62,35 @@ describe 'Museum' do
     patron_2 = Patron.new("Sally", 20)
     patron_2.add_interests("IMAX")
     patron_3 = Patron.new("Johnny", 5)
-    patron_3.add_interest("Dead Sea Scrolls")
+    patron_3.add_interests("Dead Sea Scrolls")
     dmns.admit(patron_1)
     dmns.admit(patron_2)
     dmns.admit(patron_3)
 
     expect(dmns.patrons).to eq [patron_1, patron_2, patron_3]
+  end
+
+  it 'can return a list of exhibits by interest' do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interests("Dead Sea Scrolls")
+    patron_1.add_interests("Gems and Minerals")
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interests("IMAX")
+    patron_3 = Patron.new("Johnny", 5)
+    patron_3.add_interests("Dead Sea Scrolls")
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+
+    expect(dmns.patrons_by_exhibit_interest).to eq({gems_and_minerals: [patron_1],
+                                                     dead_sea_scrolls: [patron_1, patron_3],
+                                                     imax: [patron_2]})
   end
 end
