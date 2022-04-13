1d8614fb1828dc1c32fc61861f226791afec8149
Lucas Colwell <colwellc37@gmail.com>
2022-04-13 09:47:52 -0600

    Finish museum tests

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -1,18 +1,30 @@
 require 'rspec'
+require './lib/museum'
 require './lib/patron'
 require './lib/exhibit'
 
 RSpec.describe Museum do
   before :each do
-
+    @dmns = Museum.new("Denver Museum of Nature and Science")
+    @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @imax = Exhibit.new({name: "IMAX",cost: 15})
   end
 
   it "exists" do
-    expect(@).to be_a(Museum)
+    expect(@dmns).to be_a(Museum)
   end
 
   it "has attributes" do
-
+    expect(@dmns.name).to eq("Denver Museum of Nature and Science")
+    expect(@dmns.exhibits).to eq([])
   end
 
+  it "can add exhibits" do
+    @dmns.add_exhibit(gems_and_minerals)
+    @dmns.add_exhibit(dead_sea_scrolls)
+    @dmns.add_exhibit(imax)
+
+    expect(@dmns.exhibits).to eq([@gems_and_minerals, @dead_sea_scrolls, @imax])
+  end
 end
