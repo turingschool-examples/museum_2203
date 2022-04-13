52829591352442aede527ff12ab9ce148e0f748c
Luke Swenson <LukeSwenson95@outlook.com>
2022-04-13 09:08:20 -0600

    test: added attributes test for Exhibit class

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -5,4 +5,12 @@ describe Exhibit do
 
     expect(exhibit).to be_a(Exhibit)
   end
+
+  it 'has attributes' do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+    expect(exhibit.name).to eq("Gems and Minerals")
+    expect(exhibit.cost).to eq(0)
+    
+  end
 end
