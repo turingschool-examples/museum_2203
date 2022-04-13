af4552c2c1b13a4ba70d6af5857cfc99f52f4305
“Brad <hutch13531@gmail.com>
2022-04-13 11:56:52 -0400

    Test: Patron-has readable attributes

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -7,6 +7,13 @@ RSpec.describe Patron do
   expect(patron_1).to be_an_instance_of(Patron)
   end
 
+  it 'has readable attributes' do
+    patron_1 = Patron.new("Bob", 20)
+  expect(patron_1.name).to eq("Bob")
+  expect(patron_1.spending_money).to eq(20)
+
+  end
+
 
 
 end
