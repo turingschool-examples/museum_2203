a852436e628b57751cbe3fb5e33fb57fb0cd756f
Luke Swenson <LukeSwenson95@outlook.com>
2022-04-13 09:20:27 -0600

    test: added can add interests test

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -15,4 +15,12 @@ describe Patron do
     expect(patron_1.interests).to eq([])
 
   end
+
+  it 'has can add interests' do
+    patron_1 = Patron.new("Bob", 20)
+
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    expect(patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
+  end
 end
