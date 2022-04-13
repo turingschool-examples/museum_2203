39eec5ff646c156f9ea1ebfa40ca0964fd184f2b
tjhaines-cap <tjhaines@eckerd.edu>
2022-04-13 09:29:05 -0600

    Feat: test add exhibit to museum

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -5,6 +5,10 @@ describe Museum do
 
   before(:each) do
     @dmns = Museum.new("Denver Museum of Nature and Science")
+    @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @imax = Exhibit.new({name: "IMAX",cost: 15})
+
   end
 
   it 'exists' do
@@ -14,5 +18,13 @@ describe Museum do
   it 'had readable attributes' do
     expect(@dmns.name).to eq("Denver Museum of Nature and Science")
     expect(@dmns.exhibits).to eq([])
+  end
+
+  it 'can add exhibits' do
+    @dmns.add_exhbit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhbit(@imax)
+
+    expect(@dmns.exhibits).to eq([@gems_and_minerals, @dead_sea_scrolls, @imax])
   end
 end
