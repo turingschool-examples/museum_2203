4193a5fecb83155a440821a0402a63365e10f4fd
Jim Riddle <jimriddle0@gmail.com>
2022-04-13 10:30:10 -0500

    wrote recommended exhibits method to get test to pass

--- lib/museum.rb
+++ lib/museum.rb
@@ -11,4 +11,17 @@ class Museum
     @exhibits << exhibit
   end
 
+  def recommended_exhibits(patron)
+    recommended = []
+    exhibits.each do |exhibit|
+      patron.interests.each do |interest|
+        # require 'pry'; binding.pry
+        if interest == exhibit.name
+          recommended << exhibit 
+        end
+      end
+    end
+    return recommended
+  end
+
 end
