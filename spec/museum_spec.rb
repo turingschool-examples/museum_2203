24f31808bfe6a41997b4dc32cdb27f280311a885
tjhaines-cap <tjhaines@eckerd.edu>
2022-04-13 09:34:52 -0600

    Feat: test recommend exhibit in museum

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -9,6 +9,13 @@ describe Museum do
     @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
     @imax = Exhibit.new({name: "IMAX",cost: 15})
 
+    @patron_1 = Patron.new("Bob", 20)
+    @patron_2 = Patron.new("Sally", 20)
+
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_2.add_interest("IMAX")
+
   end
 
   it 'exists' do
@@ -26,5 +33,15 @@ describe Museum do
     @dmns.add_exhibit(@imax)
 
     expect(@dmns.exhibits).to eq([@gems_and_minerals, @dead_sea_scrolls, @imax])
+  end
+
+  it 'can recommend exhibits for patrons' do
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+
+    expect(@dmns.recommend_exhibits(@patron_1)).to eq([@dead_sea_scrolls, @gems_and_minerals])
+    expect(@dmns.recommend_exhibits(@patron_2)).to eq([@imax])
   end
 end
