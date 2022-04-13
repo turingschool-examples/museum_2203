d433903938e99cf07b4049f4db8fd0463ba708e3
Stirling Hostetter <stirhoss@icloud.com>
2022-04-13 10:22:13 -0500

    Add tests for name and spending money

--- lib/patron.rb
+++ lib/patron.rb
@@ -3,4 +3,10 @@ require 'pry'
 require './lib/patron'
 
 class Patron
+  attr_reader :name, :spending_money
+
+  def initialize(name, spending_money)
+    @name = name
+    @spending_money = spending_money
+  end
 end
--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -10,4 +10,11 @@ describe 'patron' do
 
     expect(patron_1).to be_an_instance_of Patron
   end
+
+  it 'has a name and spending money' do
+    patron_1 = Patron.new("Bob", 20)
+
+    expect(patron_1.name).to eq "Bob"
+    expect(patron_1.spending_money).to eq 20
+  end
 end
