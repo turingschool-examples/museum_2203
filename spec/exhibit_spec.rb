d707f5fa68c24c7185cdbbfae77d7f9d76c3c275
Clay Ash <claycourtneyash@gmail.com>
2022-04-13 09:08:41 -0600

    Initial commit

---
+++ spec/exhibit_spec.rb
@@ -0,0 +1,10 @@
+require './lib/patron'
+require './lib/exhibit'
+
+RSpec.describe Exhibit do
+  it 'exist' do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+    expect(exhibit).to be_an_instance_of(Exhibit)
+  end
+end
