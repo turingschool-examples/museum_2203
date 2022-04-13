17045443610a0297e1ef24481d260e7be7cdcca5
Colin Reinhart <colinbreinhartgmail.com>
2022-04-13 09:53:12 -0600

    First Patron test

---
+++ spec/patron_spec.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+require './lib/exhibit'
+require './lib/patron'
+require 'pry'
+
+describe Patron do
+  before :each do
+    @patron_1 = Patron.new("Bob", 20)
+  end
+
+
+  it 'exists' do
+    expect(@patron1).to be_a(Patron)
+  end
+end
