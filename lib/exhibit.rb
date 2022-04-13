bdba1a0c97c0af9affea53b74bfb9e6ef6923744
Wyatt Kribs <kribswyatt@gmail.com>
2022-04-13 09:56:24 -0600

    exhibit class and spec complete

--- lib/exhibit.rb
+++ lib/exhibit.rb
@@ -1,9 +1,24 @@
 require './lib/patron'
 
 class Exhibit
-  attr_reader :name, :cost
-  def initialize (name, cost)
-    @name = name
-    @cost = cost
+  attr_reader :ex_name, :ex_cost, :patrons
+  def initialize (ex_name, ex_cost)
+    @ex_name = ex_name
+    @ex_cost = ex_cost
+    @patrons = []
   end
+
+  def name
+    @ex_name.fetch(:name)
+  end
+
+  def cost
+    @ex_cost.fetch(:cost)
+  end
+
+  def add_patron(patron)
+    patrons << patron
+  end
+
+
 end
--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -9,11 +9,32 @@ describe Exhibit do
 
   it "has a name" do
     exhibit = Exhibit.new({name: "Gems and Minerals"}, {cost: 0})
-    expect(exhibit.name).to eq({name: "Gems and Minerals"})
+    expect(exhibit.name).to eq("Gems and Minerals")
   end
 
   it "has a cost" do
     exhibit = Exhibit.new({name: "Gems and Minerals"}, {cost: 0})
-    expect(exhibit.cost).to eq({cost: 0})
+    expect(exhibit.cost).to eq(0)
+  end
+
+  it "has no patrons by default" do
+    exhibit = Exhibit.new({name: "Gems and Minerals"}, {cost: 0})
+    expect(exhibit.patrons).to eq([])
+  end
+
+  it "can have patrons" do
+    exhibit = Exhibit.new({name: "Gems and Minerals"}, {cost: 0})
+    patron1 = Patron.new("Bob", 20)
+    exhibit.add_patron(patron1)
+    expect(exhibit.patrons).to eq([patron1])
+  end
+
+  it "can tell us patron interests" do
+    exhibit = Exhibit.new({name: "Gems and Minerals"}, {cost: 0})
+    patron1 = Patron.new("Bob", 20)
+    exhibit.add_patron(patron1)
+    patron1.add_interest("Dead Sea Scrolls")
+    patron1.add_interest("Gems and Minerals")
+    expect(patron1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
   end
 end
