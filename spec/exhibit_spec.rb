c6617970438ae7dec8280b576f3baa4f76e8f51e
“Brad <hutch13531@gmail.com>
2022-04-13 11:39:05 -0400

    Test: exhibit-exist

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -1,7 +1,9 @@
 require './lib/exhibit'
 
 RSpec.describe Exhibit do
-  
-
+  it 'exist' do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+  expect(exhibit).to be_an_instance_of(Exhibit)
+  end
 
 end
