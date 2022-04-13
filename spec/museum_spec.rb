2d037c90dbb7b646d4d86d8a8b576d252be153dc
Jim Riddle <jimriddle0@gmail.com>
2022-04-13 10:35:03 -0500

    wrote test to admit patrons and check that they are admitted

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -63,6 +63,22 @@ RSpec.describe Museum do
 
     expect(dmns.patrons).to eq([])
 
+    patron_1 = Patron.new("Bob", 0)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("Dead Sea Scrolls")
+
+    patron_3 = Patron.new("Johnny", 5)
+    patron_3.add_interest("Dead Sea Scrolls")
+
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+
+    expect(dmns.patrons).to eq([patron_1, patron_2, patron_3])
+
   end
 
 end
