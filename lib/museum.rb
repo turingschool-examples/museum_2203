dc6abf702435134e4f87d2b324e5c814c0dbbf36
Wesley Garcia <wes@alltradestech.com>
2022-04-13 09:03:09 -0700

    Test add: check if Museum instance can display hash of patrons by exhibit interest

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,10 +1,12 @@
 class Museum
   attr_reader :name,
-    :exhibits
+    :exhibits,
+    :patrons
 
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(exhibit)
@@ -17,5 +19,9 @@ class Museum
       recommendations << exhibit if patron.interests.include?(exhibit.name)
     end
     recommendations
+  end
+
+  def admit(patron)
+    @patrons << patron
   end
 end
