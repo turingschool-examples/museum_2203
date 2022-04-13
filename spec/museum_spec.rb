a458c1a875be4f7f52f3d4513f8fe27e2e92a9ae
Wyatt Kribs <kribswyatt@gmail.com>
2022-04-13 10:04:32 -0600

    adds name test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -8,4 +8,9 @@ describe Museum do
     dmns = Museum.new("Denver Museum of Nature and Science")
     expect(dmns).to be_an_instance_of(Museum)
   end
+
+  it "has a name" do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    expect(dmns.name).to eq("Denver Museum of Nature and Science")
+  end
 end
