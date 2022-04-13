2c98bb9d7f5906e8f1a1676122e91d5b085e105b
Stephen Wilkens <stephenwilkens@gmail.com>
2022-04-13 09:21:37 -0600

    Test: add readable attributes test

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -4,6 +4,11 @@ describe Exhibit do
   let(:exhibit){Exhibit.new({name: "Gems and Minerals", cost: 0})}
 
   it "exists" do
-    expect(:exhibit).to be_a(Exhibit)
+    expect(exhibit).to be_an_instance_of(Exhibit)
+  end
+
+  it "has readable attributes" do
+    expect(exhibit.name).to eq("Gems and Minerals")
+    expect(exhibit.cost).to eq(0)
   end
 end
