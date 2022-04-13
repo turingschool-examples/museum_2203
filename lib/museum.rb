f14655045fbed77ac510e1eba4efe1d3fe32191d
Andrew King <dev.andrewking@gmail.com>
2022-04-13 11:13:28 -0500

    add admit method to add patrons

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,10 +1,11 @@
 class Museum
 
-  attr_reader :name, :exhibits
+  attr_reader :name, :exhibits, :patrons
 
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(exhibit)
@@ -17,6 +18,10 @@ class Museum
         patron_recommendations << exhibit if patron.interests.include?(exhibit.name)
     end
     patron_recommendations
+  end
+
+  def admit(patron)
+    @patrons << patron
   end
 
 end
