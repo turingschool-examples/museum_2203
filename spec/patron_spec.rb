f738f6d410824b81030b04193592a7cd15194085
Luke Swenson <LukeSwenson95@outlook.com>
2022-04-13 09:15:39 -0600

    test: created attributes test for patron class

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -5,4 +5,13 @@ describe Patron do
     expect(patron_1).to be_a(Patron)
 
   end
+
+  it "has attributes" do
+    patron_1 = Patron.new("Bob", 20)
+
+    expect(patron_1.name).to eq("Bob")
+    expect(patron_1.spending_money).to eq(20)
+    expect(patron_1.interests).to eq([])
+
+  end
 end
