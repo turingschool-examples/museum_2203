07c4b9090ded2b971c0178b5d32eebce81a7954b
Luke Swenson <LukeSwenson95@outlook.com>
2022-04-13 09:32:14 -0600

    test: added attributes test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -8,4 +8,10 @@ describe Museum do
 
     expect(dmns).to be_a(Museum)
   end
+  it "has attributes" do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    expect(dmns.name).to eq("Denver Museum of Nature and Science")
+    expect(dmns.exhibits).to eq([])
+  end
 end
