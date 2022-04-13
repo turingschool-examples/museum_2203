9f0d1c288b82d5686b858f180388ccb55c636c8f
tjhaines-cap <tjhaines@eckerd.edu>
2022-04-13 09:14:13 -0600

    Feat: test readable attributes for patron

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -11,4 +11,10 @@ describe Patron do
     expect(@patron_1).to be_a(Patron)
   end
 
+  it 'has readable attributes' do
+    expect(@patron_1.name).to eq("Bob")
+    expect(@patron_1.spending_money).to eq(20)
+    expect(@patron_1.interests)to eq([])
+  end
+
 end
