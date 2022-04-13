72284a325ad70b74050c1eeb6bfa497cdcaa0a75
Eric Espindola <eric_e_2012@hotmail.com>
2022-04-13 08:11:15 -0700

    create exhibit test file with initial test

---
+++ spec/exhibit_spec.rb
@@ -0,0 +1,9 @@
+require 'rspec'
+require './lib/exhibit'
+
+describe Exhibit do
+  it "exists" do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    expect(exhibit).to be_an_instance_of(Exhibit)
+  end
+end
