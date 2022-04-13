ce25b615d8611ad0e1c5346bd77f118071c3202e
Sarah Hall <swatson1994@yahoo.com>
2022-04-13 12:24:11 -0400

    "Feat: Add patrons by exhibit interest method and make pass"

--- lib/museum.rb
+++ lib/museum.rb
@@ -3,10 +3,11 @@ require './lib/exhibit'
 require 'pry'
 
 class Museum
-  attr_reader :name, :exhibits
+  attr_reader :name, :exhibits, :patrons
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(new_exhibit)
@@ -16,4 +17,19 @@ class Museum
   def recommend_exhibits(patron)
     @exhibits.select { |exhibit| exhibit if patron.interests.include?(exhibit.name)}
   end
+  def admit(patron)
+    @patrons << patron
+  end
+
+  def patrons_by_exhibit_interest
+    @pbei_hash = {}
+    @exhibits.each do |exhibit|
+      @pbei_hash[exhibit] = []
+      @patrons.each do |patron|
+        @pbei_hash[exhibit] << patron if recommend_exhibits(patron).include?(exhibit)
+        end
+      end
+    return @pbei_hash
+  end
+
 end
