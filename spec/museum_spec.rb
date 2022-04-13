c9f41914963738caaa1e24de62f5224ccf4609de
philmarcu <philmark18@gmail.com>
2022-04-13 09:28:05 -0600

    Start museum tests / class

---
+++ spec/museum_spec.rb
@@ -0,0 +1,30 @@
+require 'rspec'
+require 'pry'
+require './lib/exhibit'
+require './lib/patron'
+require './lib/museum'
+
+RSpec.describe Museum do
+  let(:dmns) {Museum.new("Denver Museum of Nature and Science")}
+  let(:gems_and_minerals) {Exhibit.new({name: "Gems and Minerals", cost: 0})}
+  let(:dead_sea_scrolls) {Exhibit.new({name: "Dead Sea Scrolls", cost: 10})}
+  let(:imax) {Exhibit.new({name: "IMAX",cost: 15})}
+
+  it "exists" do
+    expect(dmns).to be_an_instance_of(Museum)
+  end
+
+  it "has attributes" do
+    expect(dmns.name).to eq("Denver Museum of Nature and Science")
+    expect(dmns.exhibits).to eq([])
+  end
+
+  it "can add exhibits" do
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
+  end
+
+end
