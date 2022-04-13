3bae114346f7e2004b2ce1d637a177fffe1e93b2
tjhaines-cap <tjhaines@eckerd.edu>
2022-04-13 09:40:49 -0600

    Feat: add patrons to readable attributes test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -25,6 +25,7 @@ describe Museum do
   it 'had readable attributes' do
     expect(@dmns.name).to eq("Denver Museum of Nature and Science")
     expect(@dmns.exhibits).to eq([])
+    expect(@dmns.patrons).to eq([])
   end
 
   it 'can add exhibits' do
